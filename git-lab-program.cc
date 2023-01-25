7eaf440c3b35f8ff0d8949872da49c990927971d
leomorg <lm633020@ohio.edu>
2023-01-24 20:12:11 -0500

    Vs code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Leo Morgan>
+ *        Date: <1.24.23>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout <<"Hello , git" << endl;
+    return 0;
+}// main
